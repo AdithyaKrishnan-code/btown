9b3d3051ee365cf16ea57fad69883ea45da89b99
AdithyaKrishnan-code <jamesmacaroni69@gmail.com>
2025-01-29 16:52:11 +0530

    about_page_added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
-    "use strict";    
-    
+  "use strict";
+
   /*----------------------------------------
   Sticky Header Activation
   ------------------------------------------*/
@@ -18,8 +18,11 @@
   var mySwiper = new Swiper('.slider-container.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
-    speed: 3000,
-    autoplay: false,
+    speed: 1000, // Transition speed (1s)
+    autoplay: {
+      delay: 5000, // 5 seconds between slides
+      disableOnInteraction: false // Continue autoplay after manual interaction
+    },
     loop: true,
     effect: 'fade',
     fadeEffect: {
@@ -27,26 +30,27 @@
     },
     /* events */
     on: {
-      slideChange: function(){
+      slideChange: function () {
         var currentSlide = this.realIndex + 1;
         document.querySelector('.current-slide').innerHTML = currentSlide;
       },
-      paginationRender: function(){
+      paginationRender: function () {
         var totalSlides = document.getElementsByClassName('swiper-pagination-bullet').length;
         document.querySelector('.total-slides').innerHTML = totalSlides;
       }
     }
   });
 
+
   /*---------------------------
     Team Carousel Activation
   -----------------------------------*/
   // Swipper JS
   var mySwiper = new Swiper('.team-carousel .swiper-container', {
-    slidesPerView : 1,
+    slidesPerView: 1,
     speed: 1000,
     loop: true,
-    spaceBetween : 30,
+    spaceBetween: 30,
     autoplay: false,
     effect: 'fade',
     fadeEffect: {
@@ -63,10 +67,10 @@
   -----------------------------------*/
   // Swipper JS
   var mySwiper = new Swiper('.testimonial-carousel.swiper-container', {
-    slidesPerView : 1,
+    slidesPerView: 1,
     speed: 1000,
     loop: true,
-    spaceBetween : 30,
+    spaceBetween: 30,
     autoplay: true,
     navigation: {
       nextEl: '.testimonial-carousel .swiper-button-next',
@@ -74,42 +78,42 @@
     }
   });
 
-	/*---------------------------
+  /*---------------------------
     Magnific Popup
   -----------------------------------*/
   $('.video-popup').magnificPopup({
     type: 'iframe'
-	});
+  });
 
-	/*-----------------------------------------
+  /*-----------------------------------------
     Off Canvas Mobile Menu
   -------------------------------------------*/
   $(".header-action-btn-menu").on('click', function () {
     $("body").addClass('fix');
     $(".mobile-menu-wrapper").addClass('open');
-	});
+  });
 
-	$(".offcanvas-btn-close,.offcanvas-overlay").on('click', function () {
-		$("body").removeClass('fix');
-		$(".mobile-menu-wrapper").removeClass('open');
-	});
-	
-	/*----------------------------------------
+  $(".offcanvas-btn-close,.offcanvas-overlay").on('click', function () {
+    $("body").removeClass('fix');
+    $(".mobile-menu-wrapper").removeClass('open');
+  });
+
+  /*----------------------------------------
     Responsive Mobile Menu
   ------------------------------------------*/
   //Variables
   var $offCanvasNav = $('.mobile-menu, .category-menu'),
-  $offCanvasNavSubMenu = $offCanvasNav.find('.dropdown');
+    $offCanvasNavSubMenu = $offCanvasNav.find('.dropdown');
 
   //Close Off Canvas Sub Menu
   $offCanvasNavSubMenu.slideUp();
 
   //Category Sub Menu Toggle
-  $offCanvasNav.on('click', 'li a, li .menu-expand', function(e) {
-  var $this = $(this);
-    if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+  $offCanvasNav.on('click', 'li a, li .menu-expand', function (e) {
+    var $this = $(this);
+    if (($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand'))) {
       e.preventDefault();
-      if ($this.siblings('ul:visible').length){
+      if ($this.siblings('ul:visible').length) {
         $this.parent('li').removeClass('active');
         $this.siblings('ul').slideUp();
       } else {
@@ -125,7 +129,7 @@
   /*  When document is loading, do
   /*----------------------------------------*/
   var varWindow = $(window);
-  varWindow.on('load', function() {
+  varWindow.on('load', function () {
     AOS.init({
       once: true,
     });
@@ -139,25 +143,25 @@
   /*----------------------------------------*/
   /*  Datepicker
   /*----------------------------------------*/
-  $( ".date-pic" ).datepicker({
-    showOn:"button",
+  $(".date-pic").datepicker({
+    showOn: "button",
     buttonText: '<i class="icofont-calendar"></i>'
   });
 
   /*----------------------------------------*/
   /*  Project Filter
   /*----------------------------------------*/
-  $('.gallery-filter-nav').on( 'click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-      
-      $(this).siblings('.active').removeClass('active');
-      $(this).addClass('active');
+  $('.gallery-filter-nav').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+
+    $(this).siblings('.active').removeClass('active');
+    $(this).addClass('active');
   });
 
   var $grid = $('.gallery-filter-wrapper').isotope({
-      itemSelector: '.filter-item',
-      percentPosition: true,
+    itemSelector: '.filter-item',
+    percentPosition: true,
   });
 
   /*----------------------------------------*/
@@ -167,10 +171,10 @@
     delegate: 'a',
     type: 'image'
   });
-	/*-------------------------
+  /*-------------------------
      Ajax Contact Form 
   ---------------------------*/
-  $(function() {
+  $(function () {
 
     // Get the form.
     var form = $('#contact-form');
@@ -179,20 +183,20 @@
     var formMessages = $('.form-messege');
 
     // Set up an event listener for the contact form.
-    $(form).on('submit', function(e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
+    $(form).on('submit', function (e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
 
-        // Serialize the form data.
-        var formData = $(form).serialize();
+      // Serialize the form data.
+      var formData = $(form).serialize();
 
-        // Submit the form using AJAX.
-        $.ajax({
-          type: 'POST',
-          url: $(form).attr('action'),
-          data: formData
-        })
-        .done(function(response) {
+      // Submit the form using AJAX.
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+        .done(function (response) {
           // Make sure that the formMessages div has the 'success' class.
           $(formMessages).removeClass('error');
           $(formMessages).addClass('success');
@@ -203,16 +207,16 @@
           // Clear the form.
           $('#contact-form input,#contact-form textarea').val('');
         })
-        .fail(function(data) {
+        .fail(function (data) {
           // Make sure that the formMessages div has the 'error' class.
           $(formMessages).removeClass('success');
           $(formMessages).addClass('error');
 
           // Set the message text.
           if (data.responseText !== '') {
-              $(formMessages).text(data.responseText);
+            $(formMessages).text(data.responseText);
           } else {
-              $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
           }
         });
     });
@@ -228,19 +232,19 @@
     $window.on('scroll', function () {
       var st = $(this).scrollTop();
       if (st > $lastScrollTop) {
-          $scrollUp.removeClass('show');
+        $scrollUp.removeClass('show');
       } else {
         if ($window.scrollTop() > 200) {
-            $scrollUp.addClass('show');
+          $scrollUp.addClass('show');
         } else {
-            $scrollUp.removeClass('show');
+          $scrollUp.removeClass('show');
         }
       }
       $lastScrollTop = st;
     });
 
     $scrollUp.on('click', function (evt) {
-      $('html, body').animate({scrollTop: 0}, 600);
+      $('html, body').animate({ scrollTop: 0 }, 600);
       evt.preventDefault();
     });
   }
